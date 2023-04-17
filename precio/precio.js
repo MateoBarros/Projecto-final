9d21a2ee4ae79cec0e0276eadcb197cb6221fd4b
Mara Lloret <maralloret4@gmail.com>
2023-04-17 20:58:03 -0300

    funcionando precio kilo y torta

--- precio/precio.js
+++ precio/precio.js
@@ -1,7 +1,7 @@
-//checkbox limitado
 
-var contador = [0, 0, 0]; 
-var maxi = 3;
+//checkbox limitado
+let contador = [0, 0, 0]; 
+let maxi = 3;
 function validar(check, grupo) {
    if (check.checked==true){
        contador[grupo]++;
@@ -15,11 +15,42 @@ function validar(check, grupo) {
 }
 
 //sumar compra
-function total() {
-    try {
-    let cantidad = parseInt(document.getElementById("cantidad").value )
-    
-    document.getElementById("totalprecio").value = cantidad * 2400
-        
-    } catch (e) {}
+let cantidad1kg = document.getElementById("cantidad1kg");
+let totalprecio = document.getElementById("totalprecio");
+
+cantidad1kg.addEventListener("input",function(){
+  let numero = cantidad1kg.value;
+  let calculo = numero * 2400;
+  totalprecio.textContent = calculo;
+});
+
+// let cantidadmedio = document.getElementById("cantidadmedio");
+// let totalmedio = document.getElementById("totalmedio");
+
+// cantidadmedio.addEventListener("input",function(){
+//   let numero = cantidadmedio.value;
+//   let calculo = numero * 1500;
+//   totalprecio.textContent = calculo;
+// });
+
+//total torta
+let containertorta = document.getElementById('containertorta');
+let seleccion = containertorta.querySelectorAll('input[type="checkbox"]');
+let totaltorta = document.getElementById('totaltorta');
+let suma = 0;
+containertorta.addEventListener('change', function() {
+  suma = 0;
+  seleccion.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      suma = suma + parseInt(checkbox.value);
     }
+  });
+  totaltorta.textContent = suma;
+});
+
+//listo
+function enviar(event){
+    event.preventDefault();
+    let mensaje = document.getElementById("mensaje")
+    mensaje.innerHTML= "Su pedido fue enviado!"
+  }
